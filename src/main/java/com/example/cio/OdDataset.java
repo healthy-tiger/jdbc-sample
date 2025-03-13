3f01284b08cd9d6bf80fa443f0aeb0cb36184d33
healthy-tiger <12779326+healthy-tiger@users.noreply.github.com>
2025-03-13 23:59:56 +0900

    検索結果の読み込みと、ストリームへの書き出しを並列化してみた。

--- src/main/java/com/example/cio/OdDataset.java
+++ src/main/java/com/example/cio/OdDataset.java
@@ -14,10 +14,9 @@ import javax.json.JsonArrayBuilder;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonArray;
-import javax.json.JsonWriter;
 
 import javax.naming.InitialContext;
-import javax.sql.DataSource;
+import javax.naming.NamingException;
 
 import java.io.OutputStream;
 import java.io.IOException;
@@ -26,106 +25,128 @@ import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.nio.ByteOrder;
 
+import javax.sql.DataSource;
 import java.sql.*;
+
+import java.util.concurrent.*;
+import java.util.List;
 
 @Path("oddataset")
 public class OdDataset {
-    static JsonArray columns;
-    static {
-        columns = Json.createArrayBuilder()
-        .add("dataset_id")
-        .add("dataset_title")
-        .add("dataset_name")
-        .add("publisher")
-        .add("creator")
-        .add("group_title")
-        .add("frequency_of_update")
-        .add("description")
-        .add("release_day")
-        .build();
-    }
-
-    private void addString(JsonArrayBuilder ab, String val) {
-        if(val == null) {
-            ab.addNull();
-        } else {
-            ab.add(val);
-        }
-    }
-
-    private JsonObject row2json(ResultSet rs) throws SQLException {
-        JsonObjectBuilder ob = Json.createObjectBuilder();
-        JsonArrayBuilder ab = Json.createArrayBuilder();
-        ab.add(rs.getLong("dataset_id"));
-        addString(ab, rs.getString("dataset_title"));
-        addString(ab, rs.getString("dataset_name"));
-        addString(ab, rs.getString("publisher"));
-        addString(ab, rs.getString("creator"));
-        addString(ab, rs.getString("group_title"));
-        addString(ab, rs.getString("frequency_of_update"));
-        addString(ab, rs.getString("description"));
-        addString(ab, rs.getString("release_day"));
-        ob.add("r", ab);
-        return ob.build();
-    }
-
     private class ResponseOutput implements StreamingOutput {
         private String title;
         public ResponseOutput(String title) {
+            if(title == null || title.isEmpty()) {
+                throw new IllegalArgumentException("The title argument is either null or an empty string.");
+            }
             this.title = title;
+        }
+
+        class ResultSetReader implements Callable<Boolean> {
+            ResultSet rs;
+            Charset utf8 = Charset.forName("UTF-8");
+
+            public ResultSetReader(ResultSet rs) {
+                this.rs = rs;
+            }
+
+            public byte[] resultBytes = null;
+
+            @Override
+            public Boolean call() throws Exception {
+                resultBytes = row2json().toString().getBytes(utf8);
+                return rs.next();
+            }
+
+            private void addString(JsonArrayBuilder ab, String val) {
+                if(val == null) {
+                    ab.addNull();
+                } else {
+                    ab.add(val);
+                }
+            }
+
+            private JsonObject row2json() throws SQLException {
+                JsonObjectBuilder ob = Json.createObjectBuilder();
+                JsonArrayBuilder ab = Json.createArrayBuilder();
+                ab.add(rs.getLong("dataset_id"));
+                addString(ab, rs.getString("dataset_title"));
+                addString(ab, rs.getString("dataset_name"));
+                addString(ab, rs.getString("publisher"));
+                addString(ab, rs.getString("creator"));
+                addString(ab, rs.getString("group_title"));
+                addString(ab, rs.getString("frequency_of_update"));
+                addString(ab, rs.getString("description"));
+                addString(ab, rs.getString("release_day"));
+                ob.add("r", ab);
+                return ob.build();
+            }
+        }
+
+        class ResultSetWriter implements Callable<Boolean> {
+            OutputStream os;
+
+            public ResultSetWriter(OutputStream os) {
+                this.os = os;
+            }
+
+            public byte[] textToWrite = null;
+
+            private byte[] lenb = new byte[Integer.BYTES];
+
+            @Override
+            public Boolean call() throws Exception {
+                if(textToWrite != null) {
+                    ByteBuffer.wrap(lenb).order(ByteOrder.BIG_ENDIAN).putInt(textToWrite.length);
+                    os.write(lenb);
+                    os.write(textToWrite);
+                }
+                return true;
+            }
         }
 
         @Override
         public void write(OutputStream os) throws IOException, WebApplicationException {
-            Connection conn = null;
+            ExecutorService es = Executors.newFixedThreadPool(2);
             try {
                 InitialContext context = new InitialContext();
                 DataSource dataSource = (DataSource)context.lookup("java:/comp/env/jdbc/cio");
-                conn = dataSource.getConnection();
-
+                Connection conn = dataSource.getConnection();
                 Statement st = conn.createStatement();
-                ResultSet rs = st.executeQuery(title == "" ? "SELECT * FROM oddataset" : "SELECT * FROM oddataset WHERE dataset_title like '%" + title + "%'");
-
-                Charset utf8 = Charset.forName("UTF-8");
-                try {
-                    byte[] lenb = new byte[4];
-                    while(rs.next()) {
-                        byte[] text = row2json(rs).toString().getBytes(utf8);
-                        ByteBuffer.wrap(lenb).order(ByteOrder.BIG_ENDIAN).putInt(text.length);
-                        os.write(lenb);
-                        os.write(text);
+                ResultSet rs = st.executeQuery("SELECT * FROM oddataset WHERE dataset_title like '%" + title + "%'");
+                try (conn; st; rs) {
+                    boolean nextAvailable = rs.next();
+                    ResultSetReader reader = new ResultSetReader(rs);
+                    ResultSetWriter writer = new ResultSetWriter(os);
+                    List<Callable<Boolean>> tasks = List.of(reader, writer);
+                    while(nextAvailable) {
+                        List<Future<Boolean>> fs = es.invokeAll(tasks);
+                        writer.textToWrite = reader.resultBytes;
+                        reader.resultBytes = null;
+                        try {
+                            nextAvailable = fs.get(0).get() && fs.get(1).get();
+                        }
+                        catch(ExecutionException e) {
+                            throw e.getCause();
+                        }
                     }
                 }
-                catch(SQLException e) {
-                    throw new IOException(e);
-                }
-                finally {
-                    try {
-                        rs.close();
-                        st.close();
-                    }
-                    catch(Exception e) {
-                        throw new IOException(e);
-                    }
-                }
-
+            }
+            catch(InterruptedException e) {
+                throw new WebApplicationException(e, Response.Status.BAD_REQUEST);
             }
             catch(SQLException e) {
                 throw new WebApplicationException(e, Response.Status.BAD_REQUEST);
             }
-            catch(javax.naming.NamingException e) {
+            catch(NamingException e) {
                 throw new WebApplicationException(e, Response.Status.BAD_REQUEST);
             }
-            catch(NumberFormatException e) {
+            catch(Throwable e) {
                 throw new WebApplicationException(e, Response.Status.BAD_REQUEST);
             }
             finally {
-                try {
-                    if(conn != null) {
-                        conn.close();
-                    }
-                } catch(SQLException e) {
-                    e.printStackTrace();
+                if(es != null) {
+                    es.shutdown();
                 }
             }
         }
